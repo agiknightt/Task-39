e6a6ebf24cb059b6686c63c0d65f9e378d9a4b50
agiknightt <70384293+agiknightt@users.noreply.github.com>
2020-10-12 02:39:33 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+namespace Task_39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isExit = true;
+            Offender[] offenders = {new Offender("Иванов Сергей Валерьевич", true, 175, 70, "Русский"), new Offender("Джон Смит", false, 190, 80, "Американец"),
+                new Offender("Генадьев Людвик Аристархович", false, 160, 90, "Русский"), new Offender("Викторов Сергей Анатольевич", true, 175, 70, "Русский"), new Offender("Иванов Андрей Валерьевич", true, 160, 80, "Американец") };
+
+            while (isExit)
+            {
+                Console.WriteLine("1 - Поиск преступника по базе\n\n2 - выйти из программы\n\n");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        Console.WriteLine("Введите вес преступника : ");
+                        int weight = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Введите рост преступника : ");
+                        int height = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Введите национальность преступника : ");
+                        string nationality = Console.ReadLine();
+
+                        var newOffenders = from Offender offender in offenders where offender.Height == height && offender.Weight == weight && offender.Nationality.ToUpper() == nationality.ToUpper() && offender.InPrison != true select offender;
+
+                        foreach (var offender in newOffenders)
+                        {
+                            offender.ShowInfo();
+                        }
+                        break;
+                    case 2:
+                        isExit = false;
+                        break;
+                }
+                Console.WriteLine("Для продолжения нажмите любую клавишу...");
+                Console.ReadKey();
+                Console.Clear();                
+            }    
+        }
+    }
+        
+    
+    class Offender
+    {
+        public string FullName;
+        public bool InPrison;
+        public int Height;
+        public int Weight;
+        public string Nationality;
+        public Offender(string fullName, bool inPrison, int height, int weight, string nationality)
+        {
+            FullName = fullName;
+            InPrison = inPrison;
+            Height = height;
+            Weight = weight;
+            Nationality = nationality;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Полное имя - {FullName}\n\nрост - {Height}\n\nвес - {Weight}\n\nнациональность - {Nationality}");
+        }
+    }
+}
